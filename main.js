8221be057b1d93bebb05dcd1eb080107eba62b3d
jessyabeja <97033831+jessyabeja@users.noreply.github.com>
2022-05-23 18:05:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+song1 = "";
+song2 = "";
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+function preload()
+{
+ song1 = loadSound("peter_pan.mp3");
+ song2 = loadSound("music.mp3");
+}
+
+function setup()
+{
+ canvas = createCanvas(600,500);
+ canvas.center();
+
+ video = createCapture(VIDEO);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('Posenet is Instialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +  "leftWristY ="  + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +  "rightWristY ="  + rightWristY);
+    }
+}
+
+
+
+function draw()
+{
+ image(video, 0,0,600,500);
+
+ fill("#FF0000");
+ stroke("#FF0000")
+
+if(scoreLeftWrist > 0.2)
+{
+    circle(rightWristX, rightWristY, 20);
+    song2.stop();
+
+    
+
+
+}
+}
